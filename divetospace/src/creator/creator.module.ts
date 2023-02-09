0d5459a0ef45727aab15ec3683f13e61f10b70a3
pepperflavor <j8747j@gmail.com>
2023-02-09 18:59:26 +0900

    메타데이터 업로드 시점 변경및 db에 url 저장 완료

--- divetospace/src/creator/creator.module.ts
+++ divetospace/src/creator/creator.module.ts
@@ -5,16 +5,16 @@ import { CreatorLoginModule } from './login/login.module';
 import { UserModule } from '../user/user.module';
 import { ShinchungModule } from './shinchung/shinchung.module';
 //import { OpenfundingModule } from './openfunding/openfunding.module';
-import { RedisCacheModule } from '../cache/cache.module';
 import { HttpModule } from '@nestjs/axios';
 import { HttpService } from '@nestjs/axios';
 import { UploadsModule } from '../file-s3/uploads/uploads.module';
 import { OpenfundingModule } from './openfunding/openfunding.module';
+import { RedisCacheModule } from 'src/cache/cache.module';
 
 // OpenfundingModule
 @Module({ 
-  imports: [CreatorSignupModule, AuthModule, CreatorLoginModule, UserModule, ShinchungModule, HttpModule, OpenfundingModule, UploadsModule,
-    RedisCacheModule    
+  imports: [CreatorSignupModule, AuthModule, CreatorLoginModule, UserModule, ShinchungModule, HttpModule, OpenfundingModule, UploadsModule,RedisCacheModule
+        
   ],
   controllers: [],
   providers: []
