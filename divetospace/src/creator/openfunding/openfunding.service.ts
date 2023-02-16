f8c612e29c2997d97bb178a2144066ed8270b8bb
pepperflavor <j8747j@gmail.com>
2023-02-16 14:13:28 +0900

    크리에이터가 openfunding 실행했을때 리턴값 추가

--- divetospace/src/creator/openfunding/openfunding.service.ts
+++ divetospace/src/creator/openfunding/openfunding.service.ts
@@ -96,18 +96,19 @@ export class OpenfundingService {
             }
         })
 
-        // 여기서 ispermit 값이 변경하면 funding테이블에 fund_state 값 1로 생성
-        const metaDataurl = data.fund_pinurl;
-        // 앞단에 던져줄 데이터
-        // 토큰아이디(fund_no), 발행량, 총금액, 메타데이터url, 기간
-        const sendData = {
-            tokenId : shinNo,
-            balance : shin_nft_totalbalance,
-            totalPrice : shin_amount,
-            opendate : shin_period,
-            metaData : metaDataurl
-        }
-
+      // 여기서 ispermit 값이 변경하면 funding테이블에 fund_state 값 1로 생성
+      const metaDataurl = data.fund_pinurl;
+      const fundState = data.fund_state
+      // 앞단에 던져줄 데이터
+      // 토큰아이디(fund_no), 발행량, 총금액, 메타데이터url, 기간
+      const sendData = {
+          tokenId : shinNo,
+          balance : shin_nft_totalbalance,
+          totalPrice : shin_amount,
+          opendate : shin_period,
+          metaData : metaDataurl,
+          fundState : fundState
+      }
         // 여기서 fund_state upsert로 변경해주기
 
         // 앞단에 민팅 함수에 넣어줄 값 던져주기
